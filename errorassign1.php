376d0c5c32915fe0bbdf2cf8f27a17042330772c
Krutika <krutikaambarte23@gmail.com>
2020-05-07 21:17:30 +0530

    update

--- errorassign1.php
+++ errorassign1.php
@@ -4,9 +4,51 @@ switch ($city)
 {
     case "Mumbai":
         echo "This is mumbai city";
-    break;
+    
     case "Delhi":
         echo "This is Delhi city";
+
+    case "Nagpur":
+        echo "This is Nagpur city";
+    break;
+    default:
+    echo "No city selcted";
+
+}
+?>
+
+<?php
+echo "<br>";
+$city="Mumbai";
+switch ($city)
+{
+    case "Mumbai":
+        echo "This is mumbai city";
+    
+    case "Delhi":
+        echo "This is Delhi city";
+
+    case "Nagpur":
+        echo "This is Nagpur city";
+    break;
+    default:
+    echo "No city selcted";
+
+}
+?>
+
+<?php
+echo "<br>";
+$city="Hydrabad";
+switch ($city)
+{
+    case "Mumbai":
+        echo "This is mumbai city";
+    break;
+
+    case "Delhi":
+        echo "This is Delhi city";
+    break;
 
     case "Nagpur":
         echo "This is Nagpur city";
--- example2.php
+++ example2.php
@@ -1,5 +1,5 @@
 <?php
-$color="red";
+$color="green";
 switch ($color)
 {
 case "green":
@@ -15,3 +15,49 @@ echo "no color selected";
 
 }
 ?>
+
+
+<?php
+echo "<br>";
+$color="white";
+switch ($color)
+{
+case "green":
+echo "green color selected";
+case "red":
+echo "red color selected";
+break;
+case "yellow":
+echo  "yellow color selected";
+break;
+default:
+echo "no color selected";
+}
+?>
+
+
+
+
+
+<?php
+$color="red";
+switch ($color)
+{
+case "green":
+echo "green color selected";
+break;
+echo "<br>";
+case "red":
+echo "red color selected";
+
+case "yellow":
+echo  "yellow color selected";
+
+default:
+echo "no color selected";
+
+}
+?>
+
+
+
